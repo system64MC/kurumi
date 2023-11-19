a5d973b0fea0182bd086e82ecd51c08ba20b93e1
System64MC <system64.mc@gmail.com>
2023-11-19 23:00:27 +0100

    updated files

--- sw.js
+++ sw.js
@@ -1,22 +1,12 @@
-// Change this to your repository name
-var GHPATH = '/kurumi';
- 
-// Choose a different app prefix name
-var APP_PREFIX = 'kurwa_';
- 
-// The version of the cache. Every time you change any of the files
-// you need to change this version (version_01, version_02…). 
-// If you don't change the version, the service worker will give your
-// users the old files!
-var VERSION = 'version_01';
- 
-// The files to make available for offline use. make sure to add 
-// others to this list
+var GHPATH = '/github-page-pwa';
+var APP_PREFIX = 'gppwa_';
+var VERSION = 'version_004';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
   `${GHPATH}/index.js`,
   `${GHPATH}/index.wasm`
+  // `${GHPATH}/js/app.js`
 ]
 
 var CACHE_NAME = APP_PREFIX + VERSION
